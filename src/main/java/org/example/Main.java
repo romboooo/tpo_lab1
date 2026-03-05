d353f6532a49178cddb3e993d519b979c8493203
romboooo <412902@niuitmo.ru>
2026-03-05 18:34:39 +0300

    feat: make a bucket sort class teastable (state log)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.task2.BucketSort;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = {746, 371, 776, 321, 508, 731, 223, 287, 216, 920,
+                355, 900, 473, 714, 597, 134, 79, 674, 192, 306,
+                780, 476, 505, 954, 689, 828, 774, 589, 123, 742};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BucketSort.printArray(arr);
+        int[] newArr = BucketSort.bucketSort(arr);
+        System.out.println("________________");
+        BucketSort.printArray(newArr);
     }
 }
